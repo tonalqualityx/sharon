2233c223b3c5fa7b2e44af80177ada7a7ad7edad
relyk0159 <kyle@becomeindelible.com>
2019-06-14 16:53:47 -0400

    nightly commit

--- sharon.php
+++ sharon.php
@@ -30,6 +30,39 @@ function indppl_enqueue(){
 add_action('wp_enqueue_scripts', 'indppl_enqueue');
 
 function indppl_admin_enqueue(){
-    wp_enqueue_style('indppl-style', INDPPL_ROOT_URL . 'css/style.css');
+    wp_enqueue_style('indppl-style', INDPPL_ROOT_URL . 'css/admin-style.css');
 }
 add_action('admin_enqueue_scripts', 'indppl_admin_enqueue');
+
+function sharon_user_roles($user){
+    var_dump($_GET);
+    ?>
+    <h3>User's Organizations</h3>
+    <?php
+    $args = array(
+        'post_type' => 'organization',
+        'posts_per_page' => -1,
+    );
+    // var_dump($user);
+    var_dump(get_user_meta($user->ID));
+    $the_query = new WP_Query($args);
+    if($the_query->have_posts() ){
+        while($the_query->have_posts()){
+            $the_query->the_post();
+            $id = $the_query->post->ID;
+            $title = get_the_title();
+            $option = get_user_option($title . "_" . $id);
+            ob_start();
+            echo $option;
+            ?>
+            <input name='<?php echo $title . "_" . $id; ?>' type='checkbox' value='1' <?php checked($option, '1'); ?> /><?php echo $title; ?>
+            <?php
+            $return .= ob_get_clean();
+        }
+    }
+    echo $return;
+    ?>
+    <?php
+}
+add_action( 'show_user_profile', 'sharon_user_roles' );
+add_action( 'edit_user_profile', 'sharon_user_roles' );
