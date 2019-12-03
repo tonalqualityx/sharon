29fb269dbea67486a453401a3e4a8ef141eaf77b
relyk0159 <kyle@becomeindelible.com>
2019-12-03 12:24:53 -0500

    updated default header image

--- functions.php
+++ functions.php
@@ -72,7 +72,7 @@ function ind_display_notice($notice = false){
 add_action('wp_footer', 'ind_display_notice');
 
 function org_header_hero(){
-    $default_img = home_url() . '/wp-content/uploads/2019/05/sharon-vt-ATEF6D.jpg';
+    $default_img = home_url() . '/wp-content/uploads/2016/09/BG2.gif';
     if(has_post_thumbnail()){
         $default_img = get_the_post_thumbnail_url(get_the_id(), 'full');
     }
